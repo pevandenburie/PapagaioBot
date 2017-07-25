17f26e6a57b6e7c14502e5ee90db5e1c870f0380
Paul-Emmanuel Vandenburie <paulvand@cisco.com>
2017-07-25 13:23:28 +0200

    Answer with dummy nordestinas expressions

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
 const botBuilder = require('claudia-bot-builder'),
   fbTemplate = botBuilder.fbTemplate;
 
+let expressoes = require('./data/expressoes.json');
+
+console.log("expressoes: " + JSON.stringify(expressoes));
+
 module.exports = botBuilder( (request, apiReq) => {
 
   apiReq.lambdaContext.callbackWaitsForEmptyEventLoop = false;
 
-  return `Oxe! O que tu ta dizendo moço ?`
+  return ""+expressoes[Math.floor(Math.random() * expressoes.length)];
 });
